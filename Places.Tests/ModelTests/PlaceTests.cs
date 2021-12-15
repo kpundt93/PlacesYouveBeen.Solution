df9ac55e5b8b6b705f160e057390c546166fd099
Katie Pundt <kzpundt@gmail.com>
2021-12-15 11:32:34 -0800

    add constructor and testing for getters, setters, and list creation
    Co-Authored by Kim Brannian <brannian@gmail.com>

---
+++ Places.Tests/ModelTests/PlaceTests.cs
@@ -0,0 +1,51 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PlacesYouveBeen.Models;
+using System.Collections.Generic;
+using System;
+
+namespace PlacesYouveBeen.Tests
+{
+  [TestClass]
+  public class PlaceTests : IDisposable
+  {
+    public void Dispose()
+    {
+      Place.ClearAll();
+    }
+
+    [TestMethod]
+    public void PlaceConstructor_CreatesInstanceOfPlace_Place()
+    {
+      Place newPlace = new Place("test");
+      Assert.AreEqual(typeof(Place), newPlace.GetType());
+    }
+
+    [TestMethod]
+    public void GetCityName_ReturnsCityName_String()
+    {
+      string cityName = "Hawaii";
+      Place newPlace = new Place(cityName);
+      string result = newPlace.CityName;
+      Assert.AreEqual(cityName, result);
+    }
+
+    [TestMethod]
+    public void SetCityName_SetCityName_String()
+    {
+      string cityName = "Hawaii";
+      Place newPlace = new Place(cityName);
+      string updatedCityName = "Austin, TX";
+      newPlace.CityName = updatedCityName;
+      string result = newPlace.CityName;
+      Assert.AreEqual(updatedCityName, result);
+    }
+
+    [TestMethod]
+    public void GetAll_ReturnsEmptyList_PlaceList()
+    {
+      List<Place> newList = new List<Place> { };
+      List<Place> result = Place.GetAll();
+      CollectionAssert.AreEqual(newList, result);
+    }
+  }
+}
